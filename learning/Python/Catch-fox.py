1e23919008fd87df456a0b66d0533cc4daed9523
NaoYLee <419290065@qq.com>
2025-08-18 09:54:07 +0800

    改进了一下，现在可以计算某种检查方式的效率

--- learning/Python/Catch-fox.py
+++ learning/Python/Catch-fox.py
@@ -2,41 +2,56 @@
 # todo 5个洞，狐狸每晚随机跳向相邻的洞，每天一次检查机会
 import random
 
-# : 初始化洞穴状态
-holes = {1:'empty', 2:'empty', 3:'empty', 4:'empty', 5:'empty'}
-# : 初始化狐狸
-fox_hole = random.randint(1, 5)
-holes[fox_hole] = 'fox'
+# : 设置检查序列
+test = [int(x) for x in input('输入检查顺序（空格分隔）: ').split()]
+total_days = 0
 
-while True:
-    # : 调试
-    print(f"狐狸移动到了洞 {fox_hole}")
+# 进行10次实验
+for j in range(1, 10001):
+    # : 每次实验重新初始化
+    holes = {1:'empty', 2:'empty', 3:'empty', 4:'empty', 5:'empty'}
+    fox_hole = random.randint(1, 5)
+    holes[fox_hole] = 'fox'
+    con = 0
+    days = 1
     
-    # : 设定检查的洞穴
-    check_fox = int(input('请输入要检查的洞穴（1-5）：'))
-
-    # : 检查输入合法性
-    if check_fox > 5 or check_fox < 1:
-        print('这里没有这个洞')
-        continue
-
-    # : 抓住狐狸
-    if holes.get(check_fox) == 'fox':
-        print('你抓住狐狸了！')
-        break
-    # : 狐狸转移
-    else:
-        print('狐狸不在这个洞里')
+    # # : 调试
+    # print(f'实验 {j} - 狐狸初始位置: {fox_hole}')
+    
+    while True:
+        # : 循环使用检查序列
+        if con >= len(test):
+            con = 0
+        
+        check_fox = test[con]
+        con += 1
+        
+        # : 抓住狐狸
+        if holes[check_fox] == 'fox':
+            # print(f'第{days}天检查洞{check_fox}时抓住狐狸!')
+            break
+        
+        # : 狐狸移动
+        # print(f'第{days}天检查洞{check_fox}: 未找到')
+        old_hole = fox_hole
+        
         if fox_hole == 1:
-            holes[1] = 'empty'
-            holes[2] = 'fox'
-            fox_hole += 1
+            new_hole = 2
         elif fox_hole == 5:
-            holes[5] = 'empty'
-            holes[4] = 'fox'
-            fox_hole -= 1
+            new_hole = 4
         else:
-            change = int(random.choice([-1, 1]))
-            holes[fox_hole] = 'empty'
-            fox_hole = fox_hole + change
-            holes[fox_hole] = 'fox'
+            new_hole = fox_hole + random.choice([-1, 1])
+        
+        # : 更新洞穴状态
+        holes[old_hole] = 'empty'
+        holes[new_hole] = 'fox'
+        fox_hole = new_hole
+        # print(f'狐狸从洞{old_hole}移动到洞{new_hole}')
+        
+        days += 1
+    
+    total_days += days
+    # print(f'本次实验花费{days}天')
+
+avg_days = total_days / 10000
+print(f'平均花费{avg_days:.2f}天抓住狐狸')
